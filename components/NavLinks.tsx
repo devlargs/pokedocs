df91641a82bb5675cc851d553e33fc2ab7650a41
Ralph Largo <devlargs@gmail.com>
2020-10-18 15:08:43 +0800

    feat: create simple pagination

---
+++ components/NavLinks.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const NavLinks = () => (
+  <>
+    <Link href="/">
+      <a className="my-1 text-sm .text-gray-200 leading-5 hover:text-blue-100 hover:underline md:mx-4 md:my-0">
+        Home
+      </a>
+    </Link>
+    <Link href="/">
+      <a className="my-1 text-sm .text-gray-200 leading-5 hover:text-blue-100 hover:underline md:mx-4 md:my-0">
+        Pokemons
+      </a>
+    </Link>
+    <Link href="/">
+      <a className="my-1 text-sm .text-gray-200 leading-5 hover:text-blue-100 hover:underline md:mx-4 md:my-0">
+        About Us
+      </a>
+    </Link>
+    <Link href="/">
+      <a className="my-1 text-sm .text-gray-200 leading-5 hover:text-blue-100 hover:underline md:mx-4 md:my-0">
+        Blogs
+      </a>
+    </Link>
+  </>
+);
+
+export default NavLinks;
