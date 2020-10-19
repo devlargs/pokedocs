fcf57a575abc2df10e28ec2be829b16b15cce3e3
Ralph Largo <devlargs@gmail.com>
2020-10-19 21:31:45 +0800

    feat: added nprogress for page loading indicator | fixes #24

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,29 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Header from "components/Header";
 import "styles/global.css";
+import NProgress from "nprogress";
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const nprogressStart = () => NProgress.start();
+    const nprogressDone = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", nprogressStart);
+    router.events.on("routeChangeComplete", nprogressDone);
+    router.events.on("routeChangeError", nprogressDone);
+
+    return () => {
+      router.events.off("routeChangeStart", nprogressStart);
+      router.events.off("routeChangeComplete", nprogressDone);
+      router.events.off("routeChangeError", nprogressDone);
+    };
+  }, []);
+
   return (
     <>
       <Header />
