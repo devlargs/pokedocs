01344cf37e7427d893c6c33d5d4a6beb97e2fb9e
Ralph Largo <devlargs@gmail.com>
2021-03-24 20:32:41 +0800

    feat: added try catch handler for getStaticProps

--- pages/pokedex/[id].tsx
+++ pages/pokedex/[id].tsx
@@ -3,38 +3,10 @@ import PokeApi from "constants/pokeApi";
 import { useRouter } from "next/router";
 import { v4 as uuidv4 } from "uuid";
 
-const PokemonById = ({ name, data }) => {
+const PokemonById = ({ name, data = [] }) => {
   const { query } = useRouter();
 
   return (
-    // <div className="bg-white shadow p-4 rounded lg:w-64">
-    //   <div className="text-center mt-4">
-    //     <p className="text-gray-600 font-bold">{name}</p>
-    //     <p className="text-sm font-hairline text-gray-600 mt-1">#4</p>
-    //   </div>
-    //   <div className="flex justify-center mt-4">
-    //     <img
-    //       className="shadow sm:w-12 sm:h-12 w-10 h-10 rounded-full"
-    //       src={SAMPLE_IMAGE_LINK}
-    //       alt="Avatar"
-    //     />
-    //   </div>
-    //   <div className="mt-6 flex justify-between text-center">
-    //     <div>
-    //       <p className="text-gray-700 font-bold">20</p>
-    //       <p className="text-xs mt-2 text-gray-600 font-hairline">Attack</p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-700 font-bold">99k</p>
-    //       <p className="text-xs mt-2 text-gray-600 font-hairline">Moves</p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-700 font-bold">530</p>
-    //       <p className="text-xs mt-2 text-gray-700 font-hairline">Damage</p>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="bg-white">
       <div className="max-w-screen-xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
         <div className="lg:grid lg:grid-cols-3 lg:gap-8">
@@ -111,15 +83,26 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const { content } = await wiki().page(params.id);
-  const data = await content();
+  // TODO - refactor and add sentry
+  try {
+    const { content } = await wiki().page(params.id);
+    const data = await content();
 
-  return {
-    props: {
-      name: params.id.split("-").join(" ").toUpperCase(),
-      data,
-    },
-  };
+    return {
+      props: {
+        name: params.id.split("-").join(" ").toUpperCase(),
+        data,
+      },
+    };
+  } catch (ex) {
+    return {
+      props: {
+        name: params.id.split("-").join(" ").toUpperCase(),
+        data: [],
+        errored: true,
+      },
+    };
+  }
 }
 
 export default PokemonById;
